9d9ed81866371ca2a05906e01299854c32782907
Okoro James <okorojameschizaram@gmail.com>
2023-07-09 16:59:48 +0100

    done with about section

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+/*=============*/
+/*COUNTER CODE*/
+/*===========*/
+$(".main_section_h4").counterUp({
+  delay: 1,
+  time: 300,
+});
+
+/*=============*/
+/*OWL CAROUSEL INITIALIZATION*/
+/*===========*/
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel();
+});
