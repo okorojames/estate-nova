0c596238c1b5a901bf58f9c9f3ae844e29e8ee9a
Okoro James <okorojameschizaram@gmail.com>
2023-07-10 01:22:15 +0100

    done with properties sectionui design

--- scripts/app.js
+++ scripts/app.js
@@ -7,10 +7,15 @@ $(".main_section_h4").counterUp({
 });
 
 /*=============*/
-/*OWL CAROUSEL INITIALIZATION*/
+/*SLICK CAROUSEL INITIALIZATION*/
 /*===========*/
 $(".residence_row").slick({
   infinite: true,
   slidesToShow: 4,
   slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots: true,
+  pauseOnFocus: false,
+  pauseOnHover: false,
 });
